16ea610fc59cf8b106ff71b934abb36e84c382f2
Addison <angryoshawott@gmail.com>
2024-08-21 19:50:33 +1000

    yeah

---
+++ src/main/java/angry/shitmod/Shitmod.java
@@ -0,0 +1,18 @@
+package angry.shitmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Shitmod implements ModInitializer {
+
+	public static final String MOD_ID = "shitmod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/angry/shitmod/ShitmodClient.java
@@ -0,0 +1,10 @@
+package angry.shitmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ShitmodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
